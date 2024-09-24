64a7a176b529ad214b1fada36117a27045365e9e
gremble0 <hermanstornes@gmail.com>
2024-09-24 09:22:11 +0200

    Complete core

--- index.js
+++ index.js
@@ -54,11 +54,7 @@ const state = {
   cart: [],
 };
 
-function addToCart(item) {
-  console.log(item);
-}
-
-function createItemElement(item) {
+function createItemDescriptionElement(item) {
   const element = document.createElement("div");
 
   const elementName = document.createElement("p");
@@ -69,13 +65,76 @@ function createItemElement(item) {
   elementPrice.innerHTML = item.price;
   element.appendChild(elementPrice);
 
-  const elementButton = document.createElement("button");
-  elementButton.innerHTML = "Add to cart";
-  element.appendChild(elementButton);
+  return element;
+}
 
-  elementButton.addEventListener("click", (_) => addToCart(item));
+function createCartElement(item) {
+  const element = createItemDescriptionElement(item);
 
   return element;
+}
+
+function createButton(label, onClick) {
+  const button = document.createElement("button");
+  button.innerHTML = label;
+  button.addEventListener("click", onClick);
+  return button;
+}
+
+function createItemElement(item) {
+  const element = createItemDescriptionElement(item);
+  element.appendChild(createButton("Add to cart", (_) => addToCart(item)));
+
+  return element;
+}
+
+function addToTotal(price) {
+  const totalElement = document.querySelector(".total-number");
+  const totalPrice = parseFloat(totalElement.innerHTML.slice(1));
+  console.log(totalPrice);
+  totalElement.innerHTML = "£" + (totalPrice + price);
+}
+
+function removeFromCart(item) {
+  const elementCount = document.querySelector(
+    ".cart--item-" + item.name + "-count",
+  );
+  const itemCount = parseInt(elementCount.innerHTML.slice(1));
+  if (itemCount === 1)
+    document.querySelector(".cart--item-" + item.name).remove();
+  else elementCount.innerHTML = "x" + (itemCount - 1);
+
+  addToTotal(-item.price);
+}
+
+function addNewItemToCart(item) {
+  const element = createCartElement(item);
+  element.setAttribute("class", "cart--item-" + item.name);
+
+  const elementCount = document.createElement("p");
+  elementCount.setAttribute("class", "cart--item-" + item.name + "-count");
+  elementCount.innerHTML = "x1";
+  element.appendChild(elementCount);
+
+  element.appendChild(
+    createButton("Remove from cart", (_) => removeFromCart(item)),
+  );
+
+  document.querySelector(".cart--item-list").appendChild(element);
+}
+
+function addToCart(item) {
+  const existing = document.querySelector(".cart--item-" + item.name);
+  if (existing === null) addNewItemToCart(item);
+  else {
+    const elementCount = document.querySelector(
+      ".cart--item-" + item.name + "-count",
+    );
+    elementCount.innerHTML =
+      "x" + (parseInt(elementCount.innerHTML.slice(1)) + 1);
+  }
+
+  addToTotal(item.price);
 }
 
 const storeItemList = document.querySelector(".store--item-list");
@@ -83,4 +142,3 @@ const storeItemList = document.querySelector(".store--item-list");
 state.items.forEach((item) => {
   storeItemList.appendChild(createItemElement(item));
 });
-
